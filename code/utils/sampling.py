058c67adef2ed40a2a43fe1cf7cb680e9a95a177
Shuaiwen Wang <sw2853@columbia.edu>
2017-04-02 14:45:34 -0400

    code-utils-sampling: add sampling {haar, low rank} matrix; low rank unfinished

---
+++ code/utils/sampling.py
@@ -0,0 +1,60 @@
+import numpy as np
+import numpy.random as npr
+import numpy.linalg as npl
+import scipy as sp
+
+def haar(n, m):
+    '''
+    Return a (n * m) random orthogonal matrix following haar distribution.
+
+    argument: integer n, m; (n >= m)
+    value: a numpy array with dimension n * m
+    algorithm:
+        1. generate a (n * m) Gaussian Ensemble
+        2. QR decomposition, guarantee diagonal of R positive
+        3. Pick Q.
+        Reference: "The efficient generation of random orthogonal matrices with an application to condition estimators"
+    '''
+
+    if n < m:
+        raise ValueError("n must be as large as m.")
+
+    A = npr.normal(0, 1, (n, m))
+    Q, R = npl.qr(A, mode = 'reduced')[ : 2]
+
+    for i in xrange(m):
+        if R[i, i] < 0:
+            Q[:, i] = -Q[:, i]
+
+    return Q
+
+
+def lowRank(n, m, r, dist):
+    '''
+    Return a (n * m) low rank matrix
+    
+    argument: integer n, m, r; (n >= m; r <= min(n, m))
+    value: a numpy array with dimension (n * m)
+    description:
+        we construct the low rank matrix A based on
+            A = \sum_{i=1} ^ r lambda_i u_i v_i^T
+        where U, Lambda, V are independent with each other
+        U, V are haar orthogonal matrix with dimension (n * r), (m * r)
+        Lambda is (r * r) with diagonal i.i.d. dist
+    '''
+
+    if (n < m) or (r > min(n, m)):
+        raise ValueError("(n, m, r) should satisfy n >= m and r <= min(n, m).")
+
+    U = haar(n, r)
+    V = haar(m, r)
+
+    if type(dist) = str:
+        L = np.diag()
+    elif type(dist) = ddist:
+        L = np.diag()
+    else:
+        raise TypeError("dist must be either a string specifying the distribution, or of ddist type.")
+
+    return np.dot(U, np.dot(L, V.T))
+
